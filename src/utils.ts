9133b75935707eb06ecda6bb124ace6102ed0f69
CoreAngel <bragon912@gmail.com>
2020-12-19 22:41:00 +0100

    typo

--- src/utils.ts
+++ src/utils.ts
@@ -2,7 +2,7 @@ import crypto from 'crypto'
 
 export const randomString = (length: number = 16): string => {
   if (length <= 0) {
-    throw 'Lenght must be number more than 0'
+    throw 'Length must be number more than 0'
   }
   return crypto.randomBytes(length).toString('hex');
 }
