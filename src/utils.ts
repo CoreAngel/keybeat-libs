281fed4ca2c599725633ae23feb0db8fb8fffcef
CoreAngel <bragon912@gmail.com>
2020-12-19 22:24:41 +0100

    random string

---
+++ src/utils.ts
@@ -0,0 +1,8 @@
+import crypto from 'crypto'
+
+export const randomString = (length: number = 16): string => {
+  if (length <= 0) {
+    throw 'Lenght must be number more than 0'
+  }
+  return crypto.randomBytes(length).toString('hex');
+}
